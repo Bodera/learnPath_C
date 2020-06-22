c933f245e3633f5608d3a3642602569b3efd70a4
Bode <Bodera>
2020-06-22 11:29:49 -0300

    Inserted comments to source code

---
+++ Pointers/assets/src/003-ArraysAddressesAndPointers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    char str1[] = "Hello"; //an array is equivalent to an address
+    char *str2 = "Goodbye"; //a pointer whose value is not a string, but is an array address instead
+    printf("%d %d %s\n", &str1, str1, str1); //array address, array address, content of the array
+    printf("%d %d %s\n", &str2, str2, str2); //array address, pointer address, content of the pointer
+    return 0;
+}
